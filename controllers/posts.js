54d5ce9e5ff0f86dba915d45d4d0876a04bf6c65
MonishacloudS <monishas7122001@gmail.com>
2022-10-17 20:30:04 +0530

    initial commit

---
+++ controllers/posts.js
@@ -0,0 +1,56 @@
+import { request, response } from "express";
+import mongoose from "mongoose";
+import PostMessage from "../models/postMessage.js";
+
+export const getPosts = async (request, response) => {
+    try {
+        const postMessages = await PostMessage.find();
+        console.log(postMessages);
+        response.status(200).json(postMessages);
+    } catch (error) {
+        response.status(404).json({ message: error.message });
+    }
+
+}
+
+export const createPost = async(request, response) => {
+    const post = request.body;
+    const newPost = new PostMessage(post);
+    try {
+        await newPost.save();
+        response.status(201).json(newPost)
+        
+    } catch (error) {
+        response.status(409).json({ message: error.message });
+        
+    }
+}
+
+
+export const updatePost = async (request, response) => {
+    const { id: _id } = request.params;
+    const post = request.body;
+
+    if(!mongoose.Types.ObjectId.isValid(_id)) return response.status(404).send('No post with that id');
+
+    const updatedPost = await PostMessage.findByIdAndUpdate(_id, { ...post, _id }, { new: true});
+
+    response.json(updatedPost);
+}
+
+
+export const deletePost = async (request, response) => {
+    const { id } = request.params;
+    if (!mongoose.Types.ObjectId.isValid(id)) return response.status(404).send(`No post with id: ${id}`);
+    await PostMessage.findByIdAndRemove(id);
+    console.log('DELETE!');
+    response.json({ message: "Post deleted successfully." });
+}
+
+export const likePost = async (request, response) => {
+    const { id } = request.params;
+    if(!mongoose.Types.ObjectId.isValid(id)) return response.status(404).send('No Post with that id');
+    const post = await PostMessage.findById(id);
+    const updatedPost = await PostMessage.findByIdAndUpdate(id, { likeCount: post.likeCount + 1 }, { new: true})
+    response.json(updatedPost);
+}
---
+++ routes/posts.js
@@ -0,0 +1,19 @@
+import express, { request, response } from 'express';
+import { getPosts } from '../controllers/posts.js';
+import { createPost } from '../controllers/posts.js';
+import { updatePost, deletePost, likePost } from '../controllers/posts.js';
+
+
+
+const router = express.Router();
+
+
+//http://localhost:9000/posts
+
+router.get('/', getPosts);
+router.post('/', createPost);
+router.patch('/:id', updatePost);
+router.delete('/:id', deletePost);
+router.patch('/:id/likePost', likePost);
+
+export default router;
