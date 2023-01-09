3fbaf059698ad48d2dca858982883370d7cb9a39
MonishacloudS <monishas7122001@gmail.com>
2023-01-09 14:49:11 +0530

    msg

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ dotenv.config();
 app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors({
-  origin: '*'}))
+  origin: 'http://localhost:3000/posts'}))
 
 app.use('/posts', postRoutes);
 app.use('/user', userRouter);
